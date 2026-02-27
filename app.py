f1af497e03c306344dea9f284deb717da9730118
Philipalbino <philipanei982@gmail.com>
2026-02-27 06:03:22 -0500

    Added sitemap.xml for SEO

--- app.py
+++ app.py
@@ -68,6 +68,10 @@ def home():
 def robots():
     return app.send_static_file("robots.txt")
 
+@app.route("/sitemap.xml")
+def sitemap():
+    return app.send_static_file("sitemap.xml")
+
 # ===========================
 # Registration
 # ===========================
